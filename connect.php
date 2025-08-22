4750d1b3568a1e68e8be9c3d52c1ebeb26469cb2
Syed Shahid <syedshahid.9380@gmail.com>
2025-08-22 10:26:32 +0530

    git

--- connect.php
+++ connect.php
@@ -1,8 +1,9 @@
 <?php
-$host = "dpg-d1mc9eili9vc739c06o0-a.oregon-postgres.render.com";
-$db = "gloati_users";
+$host = "dpg-d2jd8cbuibrs73dgeqcg-a.oregon-postgres.render.com";
+$port = "5432";
+$dbname = "gloati_users";
 $user = "myadmin";
-$pass = "WNm50QgZaSDyHlrwJmktc8IaOl7jBJtI";
+$password = "XwXE4IolsAZeSmqU0qhQ9DAgEKNPjXLG";
 
 try {
     $conn = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
